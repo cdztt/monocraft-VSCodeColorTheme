4c952b3d2d373995277b686e3c0877518e7117e4
DanielFreeman <415919075@qq.com>
2022-08-04 10:00:19 +0800

    modified:   keybindings/index.js

--- keybindings/index.js
+++ keybindings/index.js
@@ -2,16 +2,23 @@ const vscode = require('vscode')
 
 function activate(context) {
     const command = vscode.commands.registerCommand('editor.action.appendComma', () => {
-        const editor = vscode.window.activeTextEditor
-        const lineNumber = editor.selection.active.line
-        const textLine = editor.document.lineAt(lineNumber)
-        const lineLength = textLine.text.trimEnd().length
+        vscode.commands.executeCommand('editor.action.trimTrailingWhitespace').then(() => {
+            const editor = vscode.window.activeTextEditor
+            const { line: lineIndex, character: characterIndex } = editor.selection.active
+            const textLine = editor.document.lineAt(lineIndex)
+            const lastCharacterIndex = textLine.range.end.character
 
-        if (textLine.text.charAt(lineLength - 1) !== ',') {
-            editor.edit((builder) => {
-                builder.insert(textLine.range.end, ',')
-            })
-        }
+            if (textLine.text.charAt(lastCharacterIndex - 1) !== ',') {
+                editor.edit((builder) => {
+                    builder.insert(textLine.range.end, ',')
+                }).then(() => {
+                    if (lastCharacterIndex === characterIndex) {//如果一开始光标就在行尾
+                        const originalPosition = new vscode.Position(lineIndex, characterIndex)
+                        editor.selection = new vscode.Selection(originalPosition, originalPosition)
+                    }
+                })
+            }
+        })
     })
 
     context.subscriptions.push(command)
