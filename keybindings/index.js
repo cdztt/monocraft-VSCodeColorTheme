e9751602e6822484b4f9b5668d1ffb9506dd5e00
DanielFreeman <415919075@qq.com>
2022-09-06 12:10:09 +0800

    modified:   keybindings/index.js

--- keybindings/index.js
+++ keybindings/index.js
@@ -1,16 +1,17 @@
 const vscode = require('vscode')
 
-function activate(context) {
-    const command = vscode.commands.registerCommand('editor.action.appendComma', () => {
-        vscode.commands.executeCommand('editor.action.trimTrailingWhitespace').then(() => {
+function append(punctuation) {
+    vscode.commands
+        .executeCommand('editor.action.trimTrailingWhitespace')
+        .then(() => {
             const editor = vscode.window.activeTextEditor
             const { line: lineIndex, character: characterIndex } = editor.selection.active
             const textLine = editor.document.lineAt(lineIndex)
             const lastCharacterIndex = textLine.range.end.character
 
-            if (textLine.text.charAt(lastCharacterIndex - 1) !== ',') {
+            if (textLine.text.charAt(lastCharacterIndex - 1) !== punctuation) {
                 editor.edit((builder) => {
-                    builder.insert(textLine.range.end, ',')
+                    builder.insert(textLine.range.end, punctuation)
                 }).then(() => {
                     if (lastCharacterIndex === characterIndex) {//如果一开始光标就在行尾
                         const originalPosition = new vscode.Position(lineIndex, characterIndex)
@@ -19,9 +20,24 @@ function activate(context) {
                 })
             }
         })
-    })
+}
 
-    context.subscriptions.push(command)
+function activate(context) {
+    const appendComma = vscode.commands.registerCommand(
+        'editor.action.appendComma',
+        () => {
+            append(',')
+        }
+    )
+
+    const appendSemicolon = vscode.commands.registerCommand(
+        'editor.action.appendSemicolon',
+        () => {
+            append(';')
+        }
+    )
+
+    context.subscriptions.push(appendComma, appendSemicolon)
 }
 
 module.exports = {
